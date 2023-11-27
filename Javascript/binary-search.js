a3b9b7a3f054c4b2139141f701014aa101da077f
ranjit681 <ranjithacharya681@gmail.com>
2023-11-28 00:18:41 +0530

    adddede

--- Javascript/binary-search.js
+++ Javascript/binary-search.js
@@ -1,3 +1,11 @@
 function binarySearch(arr, target) {
   let left = 0;
   let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    // Check if the middle element is the target
+    if (arr[mid] === target) {
+      return mid;
+    }
